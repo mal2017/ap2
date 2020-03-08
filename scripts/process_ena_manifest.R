4c5778716a8ec81628c97d39b545bddcf1300ca4
mal2017 <matt.a.lawlor@gmail.com>
2020-03-08 19:13:53 -0400

    streamlined layout allows easier addition of datasets

--- scripts/process_ena_manifest.R
+++ scripts/process_ena_manifest.R
@@ -2,12 +2,12 @@ library(tidyverse)
 library(yaml)
 
 
-manifest <- read_tsv("PRJNA301969.txt") %>%
+manifest <- read_tsv("config/PRJNA301969.txt") %>%
   dplyr::select(sample_title,fastq_ftp) %>%
   separate(fastq_ftp,into=c("r1","r2"),sep=";") %>%
   mutate(sample_title = str_replace_all(sample_title,"-","_"))
 
-read_yaml("heme-meta.yaml") %>% enframe() %>%
+read_yaml("config/heme-meta.yaml") %>% enframe() %>%
   mutate(condition = map_chr(value,`[[`,1)) %>%
   mutate(donor = map_chr(value,`[[`,2)) %>%
   dplyr::select(name, condition) %>%
@@ -16,4 +16,4 @@ read_yaml("heme-meta.yaml") %>% enframe() %>%
   group_by(sample_title) %>%
   summarize(fastq= pmap(list(r1,r2,condition), .f= function(x,y,z) list(fastq=list(r1=x,r2=y),condition=list(z,"primary")))) %>%
   deframe() %>%
-  yaml::write_yaml("PRJNA301969.yaml")
+  yaml::write_yaml("config/PRJNA301969.yaml")
